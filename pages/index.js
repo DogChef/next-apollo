6e6aff5074d2eae1bf4cf7780fbe6fa5069b2e1e
Gero <garciageronimo@outlook.com>
2020-01-23 14:58:59 -0300

    Added an apollo client and a basic query to the root page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_USERS = gql`
+  {
+    getUsers {
+      id
+      name
+    }
+  }
+`;
+
+const Users = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <h2>There are currently {data.getUsers.length} registered in the system</h2>
+  );
+};
 
 const Home = () => (
   <div>
@@ -9,7 +31,8 @@ const Home = () => (
     </Head>
 
     <h1> Welcome! </h1>
+    <Users />
   </div>
-)
+);
 
-export default Home
+export default Home;
