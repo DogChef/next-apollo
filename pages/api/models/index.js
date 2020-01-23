7f42f84eeabe3dd9806edf89af521f518309f19f
Gero <garciageronimo@outlook.com>
2020-01-23 13:18:09 -0300

    forgot to clean up files

--- pages/api/models/index.js
+++ pages/api/models/index.js
@@ -2,28 +2,30 @@ import fs from "fs";
 import path from "path";
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(`postgres://lotion:lotion@postgres:5432/lotion`);
+const sequelize = new Sequelize(
+  "postgres://lotion:lotion@postgres:5432/lotion"
+);
 const db = {};
 
 const route = `${process.cwd()}/pages/api/models`;
-fs
-  .readdirSync(route)
+fs.readdirSync(route)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== path.basename(__filename) &&
+      file.slice(-3) === ".js"
+    );
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(route, file));
+    const model = sequelize["import"](path.join(route, file));
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(modelName => {
-  console.log(`modelName ${modelName}`);
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
-console.log(db);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
